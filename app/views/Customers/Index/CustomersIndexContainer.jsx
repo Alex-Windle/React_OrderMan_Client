a073200c10bc172cb0e0024ca2ad8c07a07e3c37
Alex Windle <Alex-Windle@users.noreply.github.com>
2017-06-27 09:22:11 -0400

    Add link to customer form.

--- app/views/Customers/Index/CustomersIndexContainer.jsx
+++ app/views/Customers/Index/CustomersIndexContainer.jsx
@@ -18,7 +18,7 @@
 
 // Module imports
 import React, { Component } from 'react';
-
+import { Link } from 'react-router-dom';
 import CustomersTable from './components/CustomersTable';
 
 export default class CustomersIndexContainer extends Component {
@@ -26,6 +26,7 @@ export default class CustomersIndexContainer extends Component {
     return (
       <div>
         <h1>This is the Customers Index Container!</h1>
+        <Link to="customers/create">Add new customer</Link>
         <h3>
           This will become a protected page when we do authentication.
           <br />
